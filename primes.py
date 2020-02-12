6706b608a95d6064cbbafe0de42e81fec44a3b61
liyazhuo <yli@liyazhuo.com>
2020-02-12 19:06:52 +0400

    Added description of AKS algorithm. Also updated the first step of AKS

--- primes.py
+++ primes.py
@@ -1,4 +1,5 @@
 from functions import mul_order
+from math import log2
 #The algorithm used here is "Sieve of Eratosthenes"
 #See https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
@@ -11,12 +12,20 @@ def primes_sieve(number):
     return isprime[-1]
 
 def primes_aks(number):
-    r = 2
-    while r > 0:
-        if mul_order(r, number)>16:
-            return r
-        else:
-            r = r+1
+    #The AKS Primality test is a deterministic primality-proving algotithm.
+    #AKS is the first primality-proving algorithm to be simultaneously general, polynomial, deterministic, and unconditional.
+    #See https://en.wikipedia.org/wiki/AKS_primality_test
+    
+    if number <=1 :
+        print("Integer input needs to be greater than 1")
+        exit(0)
+    for a in range(1, number+1):
+        for b in range(2, int(log2(number))+1):
+            if number == a**b:
+                print ("number is composite")
+                exit(0)
+    
+
 
 def test(number):
     return mul_order(number, 7)
@@ -25,4 +34,5 @@ if __name__ == "__main__":
     #num_input = int(input("Please input an positive integer: "))
     #print (primes(3434351))
     #print (primes_sieve(num_input))
-    print (test(8))
+    #print (test(8))
+    print (primes_aks(16))
